e0603a580f15803624c8f3726cfbef7fff213a41
Daneswars <89680649+Daneswars@users.noreply.github.com>
2021-09-15 21:28:25 +0530

    Create Act1.c

---
+++ 3_implementation/src/Act1.c
@@ -0,0 +1,9 @@
+#include "Act1.h"
+
+void SEAT_CHECKING(){
+    DDRD|=(1<<PD0); //Set B0= 1
+    DDRD&=~(1<<PD1); //Clear bit
+    PORTD|=(1<<PD1); //Set bit
+    DDRD&=~(1<<PD2); //Clear bit
+    PORTD|=(1<<PD2); //Set bit
+}
