6dacf86b1e189bf6a411b15cad1f5f52ea2bfe29
Daneswars <89680649+Daneswars@users.noreply.github.com>
2021-09-15 22:07:18 +0530

    Update and rename Act1.c to Activity1.c

--- 3_implementation/src/Activity1.c
+++ 3_implementation/src/Activity1.c
@@ -1,9 +1,9 @@
 #include "Activity1.h"
 
 void SEAT_CHECKING(){
-    DDRD|=(1<<PD0); //Set B0= 1
+    DDRD|=(1<<PD3); //Set D3= 1
+    DDRD&=~(1<<PD0); //Clear bit
+    PORTD|=(1<<PD0); //Set bit
     DDRD&=~(1<<PD1); //Clear bit
     PORTD|=(1<<PD1); //Set bit
-    DDRD&=~(1<<PD2); //Clear bit
-    PORTD|=(1<<PD2); //Set bit
 }
