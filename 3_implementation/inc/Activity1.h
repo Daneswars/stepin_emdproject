b6392cca6f9b1111648d50f7ca15ece918b69e40
Daneswars <89680649+Daneswars@users.noreply.github.com>
2021-09-15 22:03:12 +0530

    Update and rename Act1.h to Activity1.h

--- 3_implementation/inc/Activity1.h
+++ 3_implementation/inc/Activity1.h
@@ -4,11 +4,11 @@
 #include <avr/io.h>
 #include<util/delay.h>
 
-#define SEAT_CHECK !(PIND &(1<<PD1))
-#define HEATER_ON (PIND &(1<<PD2))
-#define HEATER_OFF PORTD&=~(1<<PD2)
+#define SEAT_CHECK !(PIND &(1<<PD0))
+#define HEATER_ON (PIND &(1<<PD1))
+#define HEATER_OFF PORTD&=~(1<<PD1)
 #define LED_ON PORTD|=(1<<PD0)
-#define LED_OFF PORTD &=~(1<<PD0)
+#define LED_OFF PORTD &=~(1<<PD3)
 
 void SEAT_CHECKING();
 
