7a93fb4c5e1766d11c3aa008be7276ceaa6866bb
Daneswars <89680649+Daneswars@users.noreply.github.com>
2021-09-16 10:03:31 +0530

    Update Activity1.c

--- 3_implementation/src/Activity1.c
+++ 3_implementation/src/Activity1.c
@@ -1,4 +1,4 @@
-#include "Activity1.h"
+#include "activity1.h"
 
 void SEAT_CHECKING(){
     DDRD|=(1<<PD3); //Set D3= 1
@@ -7,3 +7,19 @@ void SEAT_CHECKING(){
     DDRD&=~(1<<PD1); //Clear bit
     PORTD|=(1<<PD1); //Set bit
 }
+while(1)
+    {
+        if  ( (!(PIND & (1<<PD0))) & (!(PIND & (1<<PD1))) )
+    {
+        PORTD |= (1<<PD3);   //Turns on the LED if both the switches are turned on by the user
+        _delay_ms(200);
+    }
+    else
+    {
+       PORTD &= ~(1<<PD3);  //Turns Off the LED
+        _delay_ms(200);
+    }
+    }
+    return 0;
+}
+
